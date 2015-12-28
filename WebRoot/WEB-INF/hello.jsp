32e065544de1372e2e60b8356c17bd19008d9ed4
nimengzeng <nimengzeng@192.168.2.100>
2015-12-28 09:23:02 +0800

    First version

---
+++ WebRoot/WEB-INF/hello.jsp
@@ -0,0 +1,7 @@
+<%@page pageEncoding="utf-8"%>
+<html>
+ <head></head>
+ <body>
+  <h1>Hello</h1>
+ </body>
+</html>
---
+++ src/com/tarena/action/HelloAction.java
@@ -0,0 +1,10 @@
+package com.tarena.action;
+
+public class HelloAction {
+	
+	public String sayHello(){
+		System.out.println("HelloAction");
+		return "ok";
+	}
+
+}
